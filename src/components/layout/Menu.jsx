5629a4689858e748a9e42484f63b78170b945605
Daniel Torres <daniel@pop-os.localdomain>
2023-06-09 20:26:50 -0300

    Initial Commit

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Menu.css';
+import { Link } from 'react-router-dom';
+
+export default props => {
+  return (
+    <aside className='Menu'>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Início</Link>
+          </li>
+          <li>
+            <Link to="/param/123">Param #01</Link>
+          </li>
+          <li>
+            <Link to="/param/legal">Param #02</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/naoexiste">Não Existe</Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  )
+}
---
+++ src/views/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from  'react-router-dom';
+
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
+
+export default props => {
+    return (
+        <div className="App">
+          <BrowserRouter>
+            <Menu />
+            <Content />
+          </BrowserRouter>
+        </div>
+    )
+}
